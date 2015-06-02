b88129b29ad063717c28779ed4241f4769be1274
n9michau <n9michau@enib.fr>
2015-06-02 16:55:27 +0200

    deuxieme commit - ajout d'une base MongoDB

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,13 @@
-import static spark.Spark.*;
+import static spark.Spark.get;
+
 
 public class HelloWorld {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        //get("/hello", (req, res) -> "Hello World");
+        // matches "GET /hello/foo" and "GET /hello/bar"
+        // request.params(":name") is 'foo' or 'bar'
+        get("/hello/:name", (request, response) -> {
+            return "Hello: " + request.params(":name");
+        });
     }
 }
