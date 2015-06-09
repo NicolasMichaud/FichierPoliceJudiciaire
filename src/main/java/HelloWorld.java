17b6464cc9fd339f7a005d6f5791ac8ce4877dd3
n9michau <n9michau@enib.fr>
2015-06-09 16:53:04 +0200

    installation de différents packages

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,9 +1,10 @@
 import static spark.Spark.get;
+import static spark.SparkBase.staticFileLocation;
 
 public class HelloWorld {
     public static void main(String[] args) {
-
-        get("/", (req, res) -> "Bonjour, veuillez vous identifier :");
+        staticFileLocation("/resources/main"); // Static files
+        get("/", (req, res) -> {return req.params("html/index.html");});
 
         //get("/hello", (req, res) -> "Hello World");
         // matches "GET /hello/foo" and "GET /hello/bar"
