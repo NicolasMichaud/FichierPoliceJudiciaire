417a7b0fae77843c38acfa22af48f03ab6e6df35
n9michau <n9michau@enib.fr>
2015-06-12 08:06:04 +0200

    création d'un lien pour l'interface côté client. Lancer le serveur Jetty puis localhost:4567/

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,10 +1,14 @@
 import static spark.Spark.get;
 import static spark.SparkBase.staticFileLocation;
 
+
 public class HelloWorld {
     public static void main(String[] args) {
-        staticFileLocation("/resources/main"); // Static files
-        get("/", (req, res) -> {return req.params("html/index.html");});
+        staticFileLocation("/"); // Static files
+        get("/", (req, res) -> {
+            //return req.params("ressources/main/angular/index.html");
+            return "<a href=http://localhost:63342/FichierPoliceJudiciaire/build/resources/main/index.html>index.html<a>";
+        });
 
         //get("/hello", (req, res) -> "Hello World");
         // matches "GET /hello/foo" and "GET /hello/bar"
