2a89366ff95febd8b3a564f830e76a06498a66e5
n9michau <n9michau@enib.fr>
2015-06-13 12:30:03 +0200

    test AngularJS avec un exemple sur les telephones

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,10 +4,11 @@ import static spark.SparkBase.staticFileLocation;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        staticFileLocation("resources/main/"); // Static files
+        staticFileLocation("resources/app/"); // Static files
         get("/", (req, res) -> {
             //return req.params("ressources/main/index.html");
-            return "<a href=http://localhost:63342/FichierPoliceJudiciaire/build/resources/main/index.html>index.html<a>";
+            //return "<a href=http://localhost:63342/FichierPoliceJudiciaire/build/resources/main/index.html>index.html<a>";
+            return "<a href=http://localhost:63342/FichierPoliceJudiciaire/build/resources/main/app/index2.html>index.html<a>";
         });
 
         //get("/hello", (req, res) -> "Hello World");
---
+++ src/main/resources/js/controllers.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Nicolas on 13/06/2015.
+ */
+var fichierPoliceJudiciaireApp = angular.module('fichierPoliceJudiciaireApp', []);
+
+fichierPoliceJudiciaireApp.controller('PhoneListCtrl', function ($scope) {
+    $scope.phones = [
+        {'name': 'Nexus S',
+            'snippet': 'Fast just got faster with Nexus S.'},
+        {'name': 'Motorola XOOM™ with Wi-Fi',
+            'snippet': 'The Next, Next Generation tablet.'},
+        {'name': 'MOTOROLA XOOM™',
+            'snippet': 'The Next, Next Generation tablet.'}
+    ];
+});
