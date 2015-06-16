a2787b8d2bf84779e1b5bb12ab57a420282bdf09
n9michau <n9michau@enib.fr>
2015-06-16 11:31:09 +0200

    incomplet pour ajout BDD

--- src/main/resources/js/controllers.js
+++ src/main/resources/js/controllers.js
@@ -12,4 +12,58 @@ fichierPoliceJudiciaireApp.controller('listeDeVilains', function ($scope) {
             'prenom': 'Camille',
             'delit': 'A été trop lente à se développer (violation de l\'article 5 du code des insecte paragraphe 227 alinéa 4'}
     ];
+    $scope.valider = function() {
+        //console.log("valider()",$scope.nom);
+        var mongoose = require('mongoose');
+        mongoose.connect('mongodb://localhost/fichiersPoliceJudiciaire');
+        var persoSchema=mongoose.Schema({
+            nom:{type:String},
+            prenom:{type:String},
+            delit:{type:String}
+        });
+        var Vilains=mongoose.model('Vilains',persoSchema);
+        var nouveauPerso=new Vilains({
+            nom:$scope.nom,
+            prenom:$scope.prenom,
+            delit:$scope.delit
+        });
+        nouveauPerso.save(function(err) {
+            if (err) {
+                console.log("erreur d'écriture")
+            }
+            else {
+                console.log("enregistrement effectué");
+            }
+        });
+    }
 });
+
+/*
+
+var sendToBDD = angular.module('sendToBDD', []);
+
+sendToBDD.controller('toBDD', function ($scope) {
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/fichiersPoliceJudiciaire');
+var persoSchema=mongoose.Schema({
+    nom:{type:String},
+    prenom:{type:String},
+    delit:{type:String}
+});
+var Vilains=mongoose.model('Vilains',persoSchema);
+var nouveauPerso=new Vilains({
+    nom:'Durand',
+    prenom:'Pierre',
+    delit:'vol de saucisson'
+});
+nouveauPerso.save(function(err) {
+    if (err) {
+        console.log("erreur d'écriture")
+    }
+    else {
+        console.log("enregistrement effectué");
+    }
+}
+}
+    */
